6baf5896f9dbcabc62d529365b3e78c80483a804
Yuliya Reutovich <yrevtovich@mail.ru>
2020-10-06 22:56:31 +0300

    fix:hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     const solution = {};
     solution.turns = Math.pow(2, disksNumber) - 1;
-    solution.seconds = solution.turns / (turnsSpeed / 3600);
+    solution.seconds = Math.floor(solution.turns / (turnsSpeed / 3600));
     return solution;
 }
