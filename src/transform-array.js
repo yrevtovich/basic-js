a0ddffb17bb5c0f1e123081e9a0aa6d3cb0d3b53
Yuliya Reutovich <yrevtovich@mail.ru>
2020-11-01 18:37:58 +0300

    fix: fix transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,24 +2,46 @@ module.exports = function transform(arr) {
     if (!Array.isArray(arr)) {
         throw new Error();
     }
-    let transformableArray = arr;
-    let transformedArray = [];
-    for (let i = 0; i < transformableArray.length; i++) {
-        if ( transformableArray[i] === '--discard-prev') {
-            if (transformedArray.length > 0 ) {
-                transformedArray.splice(transformedArray.length - 1, 1);
-            }
-        } else if (transformableArray[i] === '--double-next') {
-            if (i + 1 < transformableArray.length) {
-                transformedArray.push(transformableArray[i + 1]);
-            }
-        } else if (transformableArray[i] === '--double-prev') {
-            if ( i > 0) {
-                transformedArray.push(transformableArray[i - 1]);
-            }
-        } else if ( transformableArray[i] !== '--discard-prev' && transformableArray[i] !== '--discard-next' && transformableArray[i] !== '--double-next' && transformableArray[i] !== '--double-prev' && transformableArray[i - 1] !== '--discard-next') {
-            transformedArray.push(transformableArray[i]);
+
+    const transformedArray = [];
+    let memory;
+
+    arr.forEach((item, index, array) => {
+        switch (item) {
+            case '--discard-next':
+                memory = index + 1;
+                break;
+            case '--discard-prev':
+                if (array[index - 2] === '--discard-next') {
+                    break;
+                }
+                transformedArray.pop();
+                break;
+            case '--double-prev':  
+                const length = transformedArray.length;  
+
+                if (!length || array[index - 2] === '--discard-next') {
+                    break;
+                }
+
+                transformedArray.push(array[index - 1]);
+                break;
+            case '--double-next':
+                if (index + 1 > array.length - 1) {
+                    break;
+                }
+                transformedArray.push(array[index + 1]);
+                break;
+            default:
+                transformedArray.push(item);
+                break;
         }
-    }
+
+        if (memory === index) {
+            transformedArray.pop();
+            memory = null;
+        }
+    });
+
     return transformedArray;
 };
--- test/transform-array.test.js
+++ test/transform-array.test.js
@@ -92,6 +92,8 @@ describe('Transform array', () => {
         it.optional('control sequences work properly', () => {
             for(let i = 0; i < 50; i += 1) {
                 const { input, output } = createSample(i);
+                
+                // console.log(transform(input), input, output)
                 assert.deepStrictEqual(transform(input), output);
             }   
         });
